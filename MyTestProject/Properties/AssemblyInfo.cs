b39e2bc7ef4b77bcfeeb39df434e32174f13223b
Michael Cummings <Michael.Cummings@ascendum.com>
2019-05-09 17:00:55 -0400

    Add project files.

---
+++ MyTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a8f32f36-b9c2-464f-b09a-8a8a2c2a24f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyTestProject/Tests/CalcTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using UIAutomationClient;
+
+namespace MyTestProject.Tests
+{
+    [TestFixture]
+    class CalcTest
+    {
+        [Test]
+        public void Test1()
+        {
+            ProcessStartInfo x = new ProcessStartInfo();
+            x.FileName = @"C:\Windows\System32\calc.exe";
+            Process.Start(x);
+
+            var ROOT = new CUIAutomationClass().GetRootElement();
+
+            var prc = ROOT.FindAll(TreeScope.TreeScope_Children, new CUIAutomationClass().CreateTrueCondition());
+
+            int size = prc.Length;
+            for (int i = 0; i < prc.Length; i++)
+            {
+                string l = prc.GetElement(i).CurrentName;
+                int r = prc.GetElement(i).CurrentProcessId;
+            }
+
+
+            IUIAutomationCondition window_cond = new CUIAutomationClass().CreatePropertyCondition(UIA_PropertyIds.UIA_NamePropertyId, "Calculator");
+            var window = ROOT.FindFirst(TreeScope.TreeScope_Children, window_cond);
+            
+            IUIAutomationCondition el_cond = new CUIAutomationClass().CreatePropertyCondition(UIA_PropertyIds.UIA_AutomationIdPropertyId, "num9Button");
+
+            IUIAutomationElement el = window.FindFirst(TreeScope.TreeScope_Descendants, el_cond);
+
+            var pat = (IUIAutomationInvokePattern)el.GetCurrentPattern(UIA_PatternIds.UIA_InvokePatternId);
+            pat.Invoke();
+            
+            IUIAutomation m = new CUIAutomation();
+
+            int p = 0;
+        }
+
+    }
+}
